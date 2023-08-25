1ca985e65fc0520c47f4ded3e0a8719371781d93
KarimSagitov98 <135137958+KarimSagitov98@users.noreply.github.com>
2023-08-26 02:55:39 +0300

    Add files via upload

---
+++ 3072/colors.py
@@ -0,0 +1,50 @@
+GRID_COLOR = "#ffffff"
+EMPTY_CELL_COLOR = "#ffffff"
+SCORE_LABEL_FONT = ("Verdana", 20)
+SCORE_FONT = ("Helvetica", 32, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
+
+CELL_COLORS = {
+    3: "#f5f5f5",
+    6: "#dcdcdc",
+    12: "#d3d3d3",
+    24: "#c0c0c0",
+    48: "#a9a9a9",
+    96: "#808080",
+    192: "#696969",
+    384: "#778899",
+    768: "#708090",
+    1536: "#2f4f4f",
+    3072: "#000000"
+}
+
+CELL_NUMBER_COLORS = {
+    3: "#695c57",
+    6: "#695c57",
+    12:"#ffffff",
+    24: "#ffffff",
+    48: "#ffffff",
+    96: "#ffffff",
+    192: "#ffffff",
+    384: "#ffffff",
+    768: "#ffffff",
+    1536: "#ffffff",
+    3072: "#ffffff"
+}
+
+CELL_NUMBER_FONTS = {
+    3: ("Helvetica", 55, "bold"),
+    6: ("Helvetica", 55, "bold"),
+    12: ("Helvetica", 55, "bold"),
+    24: ("Helvetica", 50, "bold"),
+    48: ("Helvetica", 50, "bold"),
+    96: ("Helvetica", 50, "bold"),
+    192: ("Helvetica", 45, "bold"),
+    384: ("Helvetica", 45, "bold"),
+    768: ("Helvetica", 45, "bold"),
+    1536: ("Helvetica", 40, "bold"),
+    3072: ("Helvetica", 40, "bold")
+}
